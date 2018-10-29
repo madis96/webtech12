86cefc7f4f99287b5cafc312c0b01fc65aa60667
madis96 <szilard.madis@gmail.com>
2018-10-29 12:19:47 +0100

    Add functions, list pages

--- js/index.js
+++ js/index.js
@@ -2,15 +2,16 @@ $( document ).ready(function() {
     //if needed to create it automatically then uncomment it!
     //loadSideMenu();
 
-    //loadMainPage();
+    loadMainPage();
 
     //sideMenu button/img(logo) click handler
-    sideMenuButtonHandler();
-    imgClickHandler();
+    sideMenuHandler();
+    /*sideMenuButtonHandler();
+    imgClickHandler();*/
 
     //listCars table(/exit) click handler
-    carListClickHandler();
-    exitPopUpHandler();
+    //carListClickHandler();
+    //exitPopUpHandler();
 });
 
 
@@ -37,6 +38,11 @@ function imgClickHandler(){
   });
 }
 
+function sideMenuHandler(){
+  sideMenuButtonHandler();
+  imgClickHandler();
+}
+
 //handle which pageContent need to be loaded
 function setPageContent(content){
   var pageContent;
@@ -45,9 +51,11 @@ function setPageContent(content){
   {
     case "1":
       pageContent = "List all cars from database";
+      loadListCars();
       break;
     case "2":
       pageContent = "List all manufacturers from database";
+      loadListManufacturers();
       break;
     case "3":
       pageContent = "Add car to database";
@@ -59,7 +67,7 @@ function setPageContent(content){
       loadMainPage();
       break;
   }
-  $(".pageContent").html(pageContent);
+  //$(".pageContent").html(pageContent);
 }
 
 //mainPage content (img, welcomeText, and some text)
@@ -86,12 +94,40 @@ function loadAddManufacturer(){
 
 //ListCars content (table with datas from cars)
 function loadListCars(){
+  var pageContent = $(".pageContent");
+  var list = $("<table>").addClass("listAll").appendTo(pageContent);
+  var thL = $("<th>").appendTo(list);
+  var thR = $("<th>").appendTo(list);
+  $(thL).html("Manufacturer");
+  $(thR).html("Car name");
 
+  $.getJSON('cars', function (data) {
+       $.each(data, function (key, value) {
+         var row = $("<tr>").attr("id",key + 1).appendTo(list);
+         var colL = $("<td>").attr("id", key + 1 + "L").appendTo(row);
+         var colR = $("<td>").attr("id", key + 1 + "R").appendTo(row);
+         $(colL).html(value.manufacturer);
+         $(colR).html(value.name);
+      });
+      carListClickHandler();
+   });
 }
 
 //ListManufacturers content (table with datas from manufacturers)
 function loadListManufacturers(){
+  var pageContent = $(".pageContent");
+  var list = $("<table>").addClass("listAll").appendTo(pageContent);
+  var thL = $("<th>").appendTo(list);
+  $(thL).html("Manufacturer name");
 
+  $.getJSON('manufacturers', function (data) {
+       $.each(data, function (key, value) {
+         var row = $("<tr>").attr("id",key).appendTo(list);
+         var colL = $("<td>").attr("id", key + 1 + "L").appendTo(row);
+         $(colL).html(value.name);
+      });
+      manufacturersListClickHandler()
+   });
 }
 
 //if needed then here you can create it automatically
@@ -113,12 +149,16 @@ function loadSideMenu(){
   var authorDesc = $("<div>").addClass("authorDesc").appendTo(body);
     authorDesc.html("Mádi Szilárd - BQ1IXF");
   var pageContent = $("<div>").addClass("pageContent").appendTo(body);
+
+  sideMenuHandler();
 }
 
 function carListClickHandler(){
   $(".listAll tr:not(:first-child)").on("click", function(){
-    console.log("You clicked one of the car from the list! ID: " + this.id);
+    popUpRemoveOnlyClick();
+    var id = this.id;
     var body = document.getElementsByTagName("BODY");
+    var disableClick = $("<div>").addClass("hiddenBackground").appendTo(body);
     var informationPopUp = $("<div>").addClass("informationPopUp").appendTo(body);
     var exitPopUpImgBox = $("<div>").addClass("exitPopUp").appendTo(informationPopUp);
       var exitImg = $("<img>").appendTo(exitPopUpImgBox);
@@ -127,20 +167,75 @@ function carListClickHandler(){
       var img = $("<img>").appendTo(imgBox);
       $(img).attr("src", "images/logo.jpg");
     var table = $("<div>").addClass("informationTable").appendTo(informationPopUp);
-    var row = $("<tr>").appendTo(table);
-      var colL = $("<td>").appendTo(row);
-      var colR = $("<td>").appendTo(row);
-      $(colL).html("Manufacturer:");
-      $(colR).html("Opel");
-    var available = $("<div>").addClass("available").appendTo(informationPopUp);
-      $(available).html("Currently available");
+
+      $.getJSON('cars', function (data) {
+        var carData = ['manufacturer', 'name'];
+        $.each(data[id-1], function (key, value) {
+            var row = $("<tr>").appendTo(table);
+              var colL = $("<td>").appendTo(row);
+              var colR = $("<td>").appendTo(row);
+              $(colL).html(firstLetterUpperCase(key));
+              $(colR).html(value);
+          });
+       });
+    exitPopUpHandler();
   });
 
 }
 
+function firstLetterUpperCase(string){
+  var firstChar = string.substring( 0, 1 );
+  var bigFirstChar = firstChar.toUpperCase();
+  return string.replace(firstChar, bigFirstChar);
+}
+
+//close popUp if press 'X' button or click outside of the popUp
 function exitPopUpHandler(){
-  $(document).delegate('.exitPopUp', 'click', function()
-  {
-    $(".informationPopUp").remove();
+  $(".exitPopUp").on("click", function(){
+    popUpRemoveOnlyClick();
+  })
+  $(".hiddenBackground").on("click", function(){
+    popUpRemoveOnlyClick();
+  })
+}
+
+//remove popUp and the hiddenBackground
+function popUpRemoveOnlyClick(){
+  $(".informationPopUp").remove();
+  $(".hiddenBackground").remove();
+}
+
+function getAllCars(){
+  $.getJSON('cars', function (data) {
+       console.log(data);
+   })
+}
+
+function manufacturersListClickHandler(){
+  $(".listAll tr:not(:first-child)").on("click", function(){
+    popUpRemoveOnlyClick();
+    var id = this.id;
+    var body = document.getElementsByTagName("BODY");
+    var disableClick = $("<div>").addClass("hiddenBackground").appendTo(body);
+    var informationPopUp = $("<div>").addClass("informationPopUp").appendTo(body);
+    var exitPopUpImgBox = $("<div>").addClass("exitPopUp").appendTo(informationPopUp);
+      var exitImg = $("<img>").appendTo(exitPopUpImgBox);
+      $(exitImg).attr("src", "images/exit.png");
+    var imgBox = $("<div>").addClass("informationImgBox").appendTo(informationPopUp);
+      var img = $("<img>").appendTo(imgBox);
+      $(img).attr("src", "images/logo.jpg");
+    var table = $("<div>").addClass("informationTable").appendTo(informationPopUp);
+
+      $.getJSON('manufacturers', function (data) {
+        $.each(data[id-1], function (key, value) {
+            var row = $("<tr>").appendTo(table);
+              var colL = $("<td>").appendTo(row);
+              var colR = $("<td>").appendTo(row);
+              $(colL).html(firstLetterUpperCase(key));
+              $(colR).html(value);
+          });
+       });
+    exitPopUpHandler();
   });
+
 }
